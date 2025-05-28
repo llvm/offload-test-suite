973184813f9fc64b9aa735594008db35787e8fb3
Sarah Spall <sarahspall@microsoft.com>
2025-05-28 07:52:20 -0700

    fix warnings as errors

--- lib/Support/Check.cpp
+++ lib/Support/Check.cpp
@@ -14,9 +14,9 @@
 #include "llvm/Support/raw_ostream.h"
 #include <cmath>
 
-constexpr uint16_t FLOAT16_BIT_SIGN = 0x8000;
-constexpr uint16_t FLOAT16_BIT_EXP = 0x7c00;
-constexpr uint16_t FLOAT16_BIT_MANTISSA = 0x03ff;
+constexpr uint16_t Float16BitSign = 0x8000;
+constexpr uint16_t Float16BitExp = 0x7c00;
+constexpr uint16_t Float16BitMantissa = 0x03ff;
 
 // limited to float, double, and long double
 template <typename T> static bool isDenorm(T F) {
@@ -24,8 +24,8 @@ template <typename T> static bool isDenorm(T F) {
 }
 
 static bool isFloat16NAN(uint16_t Val) {
-  return (Val & FLOAT16_BIT_EXP) == FLOAT16_BIT_EXP &&
-         (Val & FLOAT16_BIT_MANTISSA) != 0;
+  return (Val & Float16BitExp) == Float16BitExp &&
+         (Val & Float16BitMantissa) != 0;
 }
 
 static bool compareDoubleULP(const double &FSrc, const double &FRef,
@@ -70,7 +70,7 @@ static bool compareFloatULP(const float &FSrc, const float &FRef,
 static bool compareFloat16ULP(const uint16_t &FSrc, const uint16_t &FRef,
                               unsigned ULPTolerance) {
   // Treat +0 and -0 as equal
-  if ((FSrc & ~FLOAT16_BIT_SIGN) == 0 && (FRef & ~FLOAT16_BIT_SIGN) == 0)
+  if ((FSrc & ~Float16BitSign) == 0 && (FRef & ~Float16BitSign) == 0)
     return true;
   if (FSrc == FRef)
     return true;
@@ -78,15 +78,15 @@ static bool compareFloat16ULP(const uint16_t &FSrc, const uint16_t &FRef,
     return isFloat16NAN(FRef) && isFloat16NAN(FSrc);
 
   // Map to monotonic ordering for correct ULP diff
-  auto toOrdered = [](uint16_t H) -> int {
-    return (H & FLOAT16_BIT_SIGN) ? (~H & 0xFFFF) : (H | 0x8000);
+  auto ToOrdered = [](uint16_t H) -> int {
+    return (H & Float16BitSign) ? (~H & 0xFFFF) : (H | Float16BitSign);
   };
 
   // 16-bit floating point numbers must preserve denorms
-  int IntFSrc = toOrdered(FSrc);
-  int IntFRef = toOrdered(FRef);
-  int Diff = IntFSrc - IntFRef;
-  unsigned int AbsDiff = Diff < 0 ? -Diff : Diff;
+  const int IntFSrc = ToOrdered(FSrc);
+  const int IntFRef = ToOrdered(FRef);
+  const int Diff = IntFSrc - IntFRef;
+  const unsigned int AbsDiff = Diff < 0 ? -Diff : Diff;
   return AbsDiff <= ULPTolerance;
 }
 
