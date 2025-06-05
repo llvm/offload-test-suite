191469b304e0b0dad7295468a2c1af22fadc1936
Sarah Spall <sarahspall@microsoft.com>
2025-06-05 14:43:18 -0700

    set enabledFeatures when creating vulkan device (#221)
    Set pEnabledFeatures and pNext in VkDeviceCreateInfo
    See:
    https://registry.khronos.org/vulkan/specs/latest/man/html/VkDeviceCreateInfo.html
    Closes #220

--- lib/API/VK/Device.cpp
+++ lib/API/VK/Device.cpp
@@ -252,6 +252,27 @@ public:
     DeviceInfo.queueCreateInfoCount = 1;
     DeviceInfo.pQueueCreateInfos = &QueueInfo;
 
+    VkPhysicalDeviceFeatures2 Features{};
+    Features.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FEATURES_2;
+    VkPhysicalDeviceVulkan11Features Features11{};
+    Features11.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_1_FEATURES;
+    VkPhysicalDeviceVulkan12Features Features12{};
+    Features12.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES;
+    VkPhysicalDeviceVulkan13Features Features13{};
+    Features13.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES;
+    VkPhysicalDeviceVulkan14Features Features14{};
+    Features14.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_4_FEATURES;
+
+    Features.pNext = &Features11;
+    Features11.pNext = &Features12;
+    Features12.pNext = &Features13;
+    Features13.pNext = &Features14;
+    Features14.pNext = NULL;
+    vkGetPhysicalDeviceFeatures2(Device, &Features);
+
+    DeviceInfo.pEnabledFeatures = &Features.features;
+    DeviceInfo.pNext = Features.pNext;
+
     if (vkCreateDevice(Device, &DeviceInfo, nullptr, &IS.Device))
       return llvm::createStringError(std::errc::no_such_device,
                                      "Could not create Vulkan logical device.");
