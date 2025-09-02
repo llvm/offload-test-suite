89a90821e7d812fce487b974fa6250cf7608ba62
Finn Plummer <mail@inbelic.dev>
2025-09-02 13:29:34 -0700

    [RootSignature] Add `StaticSampler` testing (#367)
    This pr adds testing for using a `StaticSampler` to describe a `Sampler`
    resource.
    
    It checks both default and specified parameters are generated correctly.
    
    Resolves https://github.com/llvm/offload-test-suite/issues/261.

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -48,6 +48,14 @@ tools = [
 ]
 
 
+def getHighestShaderModel(features):
+    if features == None:
+        return 6, 0
+    sm = features.get("HighestShaderModel", 6.0)
+    major, minor = str(sm).split(".")
+    return int(major), int(minor)
+
+
 def setDeviceFeatures(config, device, compiler):
     API = device["API"]
     config.available_features.add(API)
@@ -73,6 +81,11 @@ def setDeviceFeatures(config, device, compiler):
         config.available_features.add("%s-AMD" % API)
 
     config.available_features.add("%s-%s" % (compiler, API))
+
+    HighestShaderModel = getHighestShaderModel(device["Features"])
+    if (6, 6) <= HighestShaderModel:
+        # https://github.com/microsoft/DirectX-Specs/blob/master/d3d/HLSL_ShaderModel6_6.md#derivatives
+        config.available_features.add("DerivativesInCompute")
 
     if device["API"] == "DirectX":
         if device["Features"].get("Native16BitShaderOpsSupported", False):
