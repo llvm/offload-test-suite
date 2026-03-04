c12c356855f137acb2029187f005635aff3b3a35
luciechoi <ychoi0407@gmail.com>
2026-03-04 21:56:00 +0000

    Add LLVM license header

--- tools/TestGenerator/reconvergence/TestCase.h
+++ tools/TestGenerator/reconvergence/TestCase.h
@@ -1,3 +1,10 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 #ifndef TESTCASE_H_
 #define TESTCASE_H_
 
