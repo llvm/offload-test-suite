d9882333d26cab6951da7d4ace6b164de7236ed2
Manon Oomen <manon@traverseresearch.nl>
2026-05-08 14:17:37 +0200

    Remove old vertex buffer check that is no longer needed.

--- lib/API/VK/Device.cpp
+++ lib/API/VK/Device.cpp
@@ -1931,11 +1931,6 @@ public:
       // conditional on the pipeline definition.
       if (auto Err = createDepthStencil(P, IS))
         return Err;
-
-      if (P.Bindings.VertexBufferPtr == nullptr)
-        return llvm::createStringError(
-            std::errc::invalid_argument,
-            "No Vertex buffer specified for graphics pipeline.");
     }
 
     if (P.isTraditionalRaster() && P.Bindings.VertexBufferPtr) {
@@ -2740,9 +2735,10 @@ public:
                    << P.DispatchParameters.DispatchGroupCount[2] << " }\n";
     } else {
       VkDeviceSize Offsets[1]{0};
-      assert(IS.VB);
-      VkBuffer VBHandle = llvm::cast<VulkanBuffer>(*IS.VB).Buffer;
-      vkCmdBindVertexBuffers(IS.CB->CmdBuffer, 0, 1, &VBHandle, Offsets);
+      if (IS.VB) {
+        VkBuffer VBHandle = llvm::cast<VulkanBuffer>(*IS.VB).Buffer;
+        vkCmdBindVertexBuffers(IS.CB->CmdBuffer, 0, 1, &VBHandle, Offsets);
+      }
       // instanceCount must be >=1 to draw; previously was 0 which draws nothing
       vkCmdDraw(IS.CB->CmdBuffer, P.getVertexCount(), 1, 0, 0);
       llvm::outs() << "Drew " << P.getVertexCount() << " vertices.\n";
