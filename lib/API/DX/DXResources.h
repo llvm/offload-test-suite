f74f9d5f29814dd33f0658d33c3893f50b59073f
Manon Oomen <manon@traverseresearch.nl>
2026-06-12 13:44:20 +0200

    Add support for 64-bit formats. (#1302)
    DX12, Vulkan and Metal do support 64-bit formats for a couple of use
    cases like Storage(UAV) Textures.
    This PR adds the 64-bit formats that are supported to the list of
    available formats and adds the necessary code to convert to API specific
    formats.
    Note that 64-bit formats are not always supported for vertex inputs or
    sampled textures.

--- lib/API/DX/DXResources.h
+++ lib/API/DX/DXResources.h
@@ -31,8 +31,8 @@ inline D3D12_HEAP_TYPE getDXHeapType(MemoryLocation Location) {
   llvm_unreachable("All MemoryLocation cases handled");
 }
 
-inline DXGI_FORMAT getDXGIFormat(Format Format) {
-  switch (Format) {
+inline DXGI_FORMAT getDXGIFormat(Format Fmt) {
+  switch (Fmt) {
   case Format::R16Sint:
     return DXGI_FORMAT_R16_SINT;
   case Format::R16Uint:
@@ -65,6 +65,16 @@ inline DXGI_FORMAT getDXGIFormat(Format Format) {
     return DXGI_FORMAT_R32G32B32A32_UINT;
   case Format::RGBA32Float:
     return DXGI_FORMAT_R32G32B32A32_FLOAT;
+  case Format::R64Uint:
+    return DXGI_FORMAT_R32G32_UINT; // DXGI has no R64, DX12 expects R32G32
+  case Format::R64Sint:
+    return DXGI_FORMAT_R32G32_SINT; // DXGI has no R64, DX12 expects R32G32
+  case Format::RG64Uint:
+    return DXGI_FORMAT_R32G32B32A32_UINT; // DXGI has no R64G64, DX12 expects
+                                          // R32G32G32B32
+  case Format::RG64Sint:
+    return DXGI_FORMAT_R32G32B32A32_SINT; // DXGI has no R64G64, DX12 expects
+                                          // R32G32G32B32
   case Format::D32Float:
     return DXGI_FORMAT_D32_FLOAT;
   case Format::D32FloatS8Uint:
