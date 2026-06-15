bd23297f357b0ebd3e454375dd522343a250a50e
Justin Bogner <mail@justinbogner.com>
2026-06-15 11:30:54 -0700

    Use the internal lit shell (#1313)
    The external lit shell is deprecated as of llvm/llvm-project#203689

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -19,7 +19,7 @@ from lit.llvm.subst import ToolSubst
 config.name = "OffloadTest-" + config.offloadtest_suite
 
 # testFormat: The test format to use to interpret tests.
-config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
+config.test_format = lit.formats.ShTest()
 
 # suffixes: A list of file extensions to treat as test files. This is overriden
 # by individual lit.local.cfg files in the test subdirectories.
