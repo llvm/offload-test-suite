74ac45c94e3c5e4109673adf9204003196bbc707
Justin Bogner <mail@justinbogner.com>
2026-08-19 13:21:20 -0700

    [ci_results] Use strptime instead of fromisoformat (#1456)
    Apparently `fromisoformat` got smarter in python 3.11. Use strptime for
    compatibility with older python, since we know the exact format of
    github's dates anyway.

--- utils/ci_results.py
+++ utils/ci_results.py
@@ -422,7 +422,7 @@ def get_last_run(workflow, status="completed"):
     for _ in range(5):
         # The gh cli seems to sometimes just return results from a month ago.
         # Work around it by trying again a few times if the results seem stale.
-        ran_at = dt.datetime.fromisoformat(last_run["createdAt"])
+        ran_at = parse_github_timestamp(last_run["createdAt"])
         if dt.datetime.now(ran_at.tzinfo) - ran_at < dt.timedelta(days=1):
             break
         last_run = get_last_run_internal(workflow, status)
@@ -463,7 +463,7 @@ def get_recent_runs(workflow, *, run_limit):
     for _ in range(5):
         # The gh cli seems to sometimes just return results from a month ago.
         # Work around it by trying again a few times if the results seem stale.
-        ran_at = dt.datetime.fromisoformat(recent_runs[0]["createdAt"])
+        ran_at = parse_github_timestamp(recent_runs[0]["createdAt"])
         if dt.datetime.now(ran_at.tzinfo) - ran_at < dt.timedelta(days=1):
             break
         recent_runs = get_recent_runs_internal(workflow, run_limit=run_limit)
@@ -494,6 +494,10 @@ def get_recent_runs_internal(workflow, *, run_limit):
     if not output:
         return None
     return json.loads(output)
+
+
+def parse_github_timestamp(timestamp):
+    return dt.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S%z")
 
 
 def get_log_proc(databaseId):
